7b380ca6ff995488b3e07fd8af21854fd1b29ac7
JeffreyDinackus <jeffreydinackus@proton.me>
2023-04-12 17:52:54 -0400

    made some different kinds of componenets for practice

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -2,8 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-  return <h1>XD</h1>;
+  return (
+    <>
+      <div className='test'>
+        <h1>XD</h1>
+        <ul>
+          <li>
+            <a href='https://facebook.com'>facebook.com</a>
+          </li>
+        </ul>
+      </div>
+      <h2>Ahoy!</h2>
+    </>
+  );
 }
+// function Greeting() {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {}, 'hello world')
+//   );
+// }
 
 export default Greeting;
 
